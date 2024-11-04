fc37dfee9a18bb6216a14523160500f7784e90fe
Idris Azim <edrisazim25@gmail.com>
2024-11-04 09:38:32 +0300

    FIle for  testing some stuff

---
+++ app/src/main/java/com/example/calculator/testing.kt
@@ -0,0 +1,14 @@
+package com.example.calculator
+
+// I use this file to test some functionalities without having to wait for the emulator
+// fyi I have a potato PC
+
+fun main(){
+    var x = "1"
+    var y = "2"
+    var z = x + y
+    var xx = x.toInt()
+    var yy = y.toInt()
+    println(z)
+    println(xx + yy)
+}
